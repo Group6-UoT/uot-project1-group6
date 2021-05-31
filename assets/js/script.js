c76cb355aff6a70961cf8ca2c42e398c91f9e067
kirti18patel <kirti18patel@yahoo.com>
2021-05-31 13:13:19 -0400

    province wise data filtered

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,69 @@ var fun = function(){
     });
 }
 fun();
+var displayProvinceData = function(){
+    //province wise data
+    fetch("https://api.opencovid.ca/timeseries")
+        .then( function(response){
+            return response.json();
+        })
+        .then(function(data){
+            // give total active recovered and death by province
+            // console.log(data);
+
+            var validation=true;
+            var userInput = document.getElementById("in").value;
+            console.log(userInput);
+            var a =data.recovered;
+            var totalRecovered=0;
+            var sumRecovered;
+            for(var i =0; i<a.length; i++){
+                // console.log(a[i].recovered);
+                if (userInput === a[i].province){
+                    totalRecovered = a[i].cumulative_recovered;
+                    validation=false;
+                    }
+                    sumRecovered= sumRecovered+a[i].recovered;
+            }
+            
+            console.log("recovered", totalRecovered);
+            if(validation === true){
+                alert("You entered wrong province name");
+                location.reload(true);
+            }
+
+            var b = data.active;
+            var totalActiveCases=0;
+            for(var i =0; i<b.length; i++){
+            if (userInput === b[i].province){
+                totalActiveCases = b[i].cumulative_cases;
+            }
+            }
+            console.log("active ",totalActiveCases);
+
+            var totalDeath=0;
+            var c= data.mortality;
+            for(var i =0; i<c.length; i++){
+                if (userInput === c[i].province){
+                    totalDeath = c[i].cumulative_deaths;
+                    }
+            }
+            console.log("death" ,totalDeath);
+
+            var totalVaccinated=0;
+            
+            var d= data.dvaccine;
+            for(var i =0; i<d.length; i++){
+                if (userInput === d[i].province){
+                    totalVaccinated = d[i].cumulative_dvaccine;
+                }
+            }
+            
+            console.log("vaccine" ,totalVaccinated);
+            document.querySelector("#data").textContent = "vaccine >" + totalVaccinated + " active > " + totalActiveCases + " recovered > " + totalRecovered + " death > " + totalDeath;
+
+        });
+
+}
+
+document.querySelector("button").addEventListener("click",displayProvinceData);
