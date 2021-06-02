e36bda30bea23b3f2a774aad26189ec65acf1491
Sona <sonali23.1293@gmail.com>
2021-06-02 13:14:20 -0400

    canada-data

--- assets/js/canada.js
+++ assets/js/canada.js
@@ -1,40 +1,19 @@
 var ctx = document.getElementById('myChart');
-//var worldChart = document.getElementById('world-data');
 var canadaChart = document.getElementById('canada-data');
 
 
-var getCaData = function(country){
+var getCaData = function(){
 
-    //var coviUrl = "https://disease.sh/v3/covid-19/countries/" + country;
-    //var coviUrl = "https://disease.sh/v3/covid-19/all";
     var coviUrl = "https://disease.sh/v3/covid-19/countries/Canada";
     fetch(coviUrl).then(function(response){
         response.json().then(function(data){
-            // casesEl = data.cases;
-            // console.log(casesEl,data.country);
-             console.log(data.active,data.country);
-
-            // var a=[];
-            // var c=[];
-            // var b=[];
-            // console.log(data);
-            // for(var i=0 ; i<data.length; i++){
-            //     a.push(data[i].cases);
-            //     c.push(data[i].country);
-
-            // }
-
-            //map(a,c);
-
-            
             var myChart = new Chart(ctx, {
                 type: 'bar',
                 data: {
-                    labels: ['Total Cases', 'Recovered', 'Active', 'Deaths'],
+                    labels: ['Total Cases', 'Recovered', 'Active', 'Serious Critical', 'Total Deaths'],
                     datasets: [{
                         label: 'Covid Data',
-                        //data: [data[i].cases, data[i].active, data[i].recovered, data[i+1].cases, 2, 3],
-                        data:[data.cases, data.recovered, data.active, data.deaths],
+                        data:[data.cases, data.recovered, data.active, data.critical, data.deaths],
                         backgroundColor: [
                             'rgba(255, 99, 132, 0.2)',
                             'rgba(54, 162, 235, 0.2)',
@@ -72,6 +51,5 @@ var getCaData = function(country){
     });
 
 };
-
 
 canadaChart.addEventListener("click",getCaData());
